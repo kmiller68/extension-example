2d65897bf234532650780e1802908ecea3324733
Keith Miller <kdm@fb.com>
2014-06-19 10:05:59 -0700

    updated composer json and lock

--- ext_example.php
+++ ext_example.php
@@ -1,3 +1,10 @@
 <?hh
 <<__Native>>
 function example_sum(int $a, int $b): int;
+
+function logit() {
+  $log = new Monolog\Logger('ext_exmaple');
+  $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+
+  $log->addWarning('Foo');
+}
